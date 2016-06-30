41a9389ef45b811c004cd6e5db50e4fac1233ced
erichess <ehess84@gmail.com>
2016-06-29 17:38:22 -0700

    changing comments

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ function serverRunning(req, res){
 //Create a server
 var server = http.createServer(serverRunning);
 
-//Lets start our server
+//Start Server
 server.listen(port, function(){
-    //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", port);
 }); 
