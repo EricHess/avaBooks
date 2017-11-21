6362e35e1f520a261940d3c3987954c7eb5fec06
erichess <ehess84@gmail.com>
2017-11-20 22:08:06 -0800

    adding userId pass through

--- routers/book.js
+++ routers/book.js
@@ -1,19 +1,25 @@
 var express = require("express");
 var router = express.Router();
+const bodyParser= require('body-parser')
 var mongo = require('mongodb'),
     Server = mongo.Server,
     Db = mongo.Db,
     ObjectID = require('mongodb').ObjectID;
-
+router.use(bodyParser.json());
+const context = {}
 
 router.get('/:id', (req, res) => {
   
   var bookId = new ObjectID(req.params.id);
 
+  //TODO: NEED TO REMOVE FROM QUERY PARAMS
+  context["userId"] = req.query.userId
+
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
+    context["results"] = results;
     //TODO: Need to update to send (or cookie) userId
-    res.render("read.ejs", {context: results})
+    res.render("read.ejs", {context:context})
 
   });
 });
@@ -24,6 +30,9 @@ router.post('/read/:id', (req, res) => {
   //TODO: Need to update user table to include booksRead collection with _id and pagesRead
 
   var bookId = new ObjectID(req.params.id);
+
+  //TODO: NEED TO GET USER ID HERE
+  db.collection('user').find({_id:"_______ADD USER ID HERE_______"})
 
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
