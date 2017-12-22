4e7ec1e8be0d21044253de6ddc986e13e19f8263
erichess <ehess84@gmail.com>
2017-12-22 14:56:59 -0800

    now passing userId through context

--- routers/book.js
+++ routers/book.js
@@ -8,12 +8,11 @@ var mongo = require('mongodb'),
 router.use(bodyParser.json());
 const context = {}
 
-router.get('/:id', (req, res) => {
+router.post('/:id', (req, res) => {
   
   var bookId = new ObjectID(req.params.id);
 
-  //TODO: NEED TO REMOVE FROM QUERY PARAMS
-  context["userId"] = req.query.userId
+  context["userId"] = req.body.userId
 
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
@@ -30,13 +29,39 @@ router.post('/read/:id', (req, res) => {
   //TODO: Need to update user table to include booksRead collection with _id and pagesRead
 
   var bookId = new ObjectID(req.params.id);
+  context["userId"] = req.body.userId;
+
+
+    var dbPromise = new Promise((resolve, reject)=>{
+
+    db.collection('users').find({_id:context["userId"]}).toArray((err, results) =>{
+      if(err) return console.log(err);
+    
+      //If the mainContext.users is created, resolve the promise (Go to line 43)
+      if(context.userId){
+        resolve(context.userId);
+      }else{
+        reject("No users found");
+      }
+
+  });
+});
+
+    dbPromise.then(
+    (msg)=>{
+      console.log(msg)
+    }, 
+    (errorMsg)=>{
+      console.log(errorMsg)
+  })
 
   //TODO: NEED TO GET USER ID HERE
-  db.collection('user').find({_id:"_______ADD USER ID HERE_______"})
+  
 
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
-    res.render("read.ejs", {context: results})
+    context["results"] = results;
+    res.render("doneReading.ejs", {context: context}) 
 
   });
 });
