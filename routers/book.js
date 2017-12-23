bb4e186a8ec25ccd09544dd55d4f0d1083882ade
erichess <ehess84@gmail.com>
2017-12-22 19:11:27 -0800

    Getting pageHistory set up through DB

--- routers/book.js
+++ routers/book.js
@@ -8,15 +8,26 @@ var mongo = require('mongodb'),
 router.use(bodyParser.json());
 const context = {}
 
+
+/***** START READING PAGE *****/
 router.post('/:id', (req, res) => {
   
-  var bookId = new ObjectID(req.params.id);
+  const bookId = new ObjectID(req.params.id);
+  const userId = new ObjectID(req.body.userId);
 
-  context["userId"] = req.body.userId
+  //Query user database to find this book ID, and then return the number of pages read from that book.
+  //IF IT DOES NOT EXIST, CREATE BOOK_INFO OBJECT WITH BOOK ID (BOOKINFO.BOOKID = BOOKID)
+  //IF IT DOES EXIST ALREADY, DO NOTHING AND NOT OVERRIDE
+  db.collection("users").update(
+    {"_id": userId},
+    {$set:{'bookInfo.bookId':bookId}}, 
+      {upsert: true}
+    ) 
 
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
     context["results"] = results;
+    context["userId"] = userId;
     //TODO: Need to update to send (or cookie) userId
     res.render("read.ejs", {context:context})
 
@@ -24,43 +35,52 @@ router.post('/:id', (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+/****** DONE READING PAGE ******/
 router.post('/read/:id', (req, res) => {
   //TODO: Coming from book page, I have read: X amount of pages
   //TODO: Need to update user table to include booksRead collection with _id and pagesRead
 
   var bookId = new ObjectID(req.params.id);
-  context["userId"] = req.body.userId;
-
+  console.log(req.body)
+  context["pagesRead"] = req.body.pagesRead;
+  const userObjectId = new ObjectID(req.body.userId);
 
     var dbPromise = new Promise((resolve, reject)=>{
+      db.collection('users').find({"_id": userObjectId}).toArray((err, results) =>{
+        if(err) reject(err);
+        resolve(results);
+      });
+    });
 
-    db.collection('users').find({_id:context["userId"]}).toArray((err, results) =>{
-      if(err) return console.log(err);
-    
-      //If the mainContext.users is created, resolve the promise (Go to line 43)
-      if(context.userId){
-        resolve(context.userId);
-      }else{
-        reject("No users found");
-      }
-
-  });
-});
-
-    dbPromise.then(
-    (msg)=>{
-      console.log(msg)
-    }, 
-    (errorMsg)=>{
-      console.log(errorMsg)
-  })
-
-  //TODO: NEED TO GET USER ID HERE
+    dbPromise.then(function(result){
+      context["userInfo"] = result[0];
+    }).then(function(){
+      //DO OPERATION HERE FOR ADDING THE NUMBER OF PAGES READ
+      //for bookId, do math to increment and add start and stop by date
+      db.collection('users').update(
+         {"_id":userObjectId,"bookInfo.bookId":bookId },
+         { $addToSet:{
+          "bookInfo.pageHistory":{
+              "pagesRead": context.pagesRead,
+              "dateRead": new Date()
   
+            }
+            }
+          } )
+       }
+       
+      )
 
   db.collection('books').find({_id:bookId}).toArray((err, results) =>{
     if(err) return console.log(err);
-    context["results"] = results;
+    context["book"] = results[0];
     res.render("doneReading.ejs", {context: context}) 
 
   });
@@ -68,3 +88,8 @@ router.post('/read/:id', (req, res) => {
 
 
 module.exports = router
+
+
+// FOR SPECIFIC USER ID, WHERE THE BOOKID ALSO MATCHES
+// ADD NEW CHILD OBJECT UNDER PAGE HISTORY
+// ADD A NEW OBJECT WITH NEW PAGES READ AND DATEOFREADING
